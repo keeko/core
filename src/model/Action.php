044fc8dec6b09c265269b289f7e222a0f3643116
gossi <github@gos.si>
2014-11-11 12:18:54 +0100

    And another new propel object

---
+++ src/model/Action.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace keeko\core\model;
+
+use keeko\core\model\Base\Action as BaseAction;
+
+/**
+ * Skeleton subclass for representing a row from the 'kk_action' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Action extends BaseAction
+{
+
+}
