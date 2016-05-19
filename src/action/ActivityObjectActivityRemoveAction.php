e33446f8128e4f5446e6cb68dcf2cef2ae02949a
gossi <github@gos.si>
2016-05-19 17:15:58 +0200

    update generated code

---
+++ src/action/ActivityObjectActivityRemoveAction.php
@@ -0,0 +1,45 @@
+<?php
+namespace keeko\core\action;
+
+use keeko\framework\foundation\AbstractAction;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use phootwork\json\Json;
+use Tobscure\JsonApi\Exception\InvalidParameterException;
+use keeko\core\domain\ActivityObjectDomain;
+
+/**
+ * Removes activity as relationship of activity_object
+ * 
+ * This code is automatically created. Modifications will probably be overwritten.
+ * 
+ * @author gossi
+ */
+class ActivityObjectActivityRemoveAction extends AbstractAction {
+
+	/**
+	 * @param OptionsResolver $resolver
+	 */
+	public function configureParams(OptionsResolver $resolver) {
+		$resolver->setRequired(['id']);
+	}
+
+	/**
+	 * Automatically generated run method
+	 * 
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function run(Request $request) {
+		$body = Json::decode($request->getContent());
+		if (!isset($body['data'])) {
+			throw new InvalidParameterException();
+		}
+		$data = $body['data'];
+		$id = $this->getParam('id');
+		$domain = new ActivityObjectDomain($this->getServiceContainer());
+		$payload = $domain->removeActivity($id, $data);
+		return $this->responder->run($request, $payload);
+	}
+}
---
+++ src/responder/ActionApiJsonResponder.php
@@ -0,0 +1,67 @@
+<?php
+namespace keeko\core\responder;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use keeko\framework\foundation\AbstractPayloadResponder;
+use keeko\framework\domain\payload\NotValid;
+use keeko\framework\exceptions\ValidationException;
+use keeko\framework\domain\payload\NotFound;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use keeko\framework\domain\payload\Updated;
+use keeko\core\model\Action;
+use keeko\framework\domain\payload\NotUpdated;
+
+/**
+ * Automatically generated JsonResponder for Reads the relationship of action to api
+ * 
+ * @author gossi
+ */
+class ActionApiJsonResponder extends AbstractPayloadResponder {
+
+	/**
+	 * @param Request $request
+	 * @param NotFound $payload
+	 */
+	public function notFound(Request $request, NotFound $payload) {
+		throw new ResourceNotFoundException($payload->getMessage());
+	}
+
+	/**
+	 * @param Request $request
+	 * @param NotUpdated $payload
+	 */
+	public function notUpdated(Request $request, NotUpdated $payload) {
+		return new JsonResponse(null, 204);
+	}
+
+	/**
+	 * @param Request $request
+	 * @param NotValid $payload
+	 */
+	public function notValid(Request $request, NotValid $payload) {
+		throw new ValidationException($payload->getViolations());
+	}
+
+	/**
+	 * @param Request $request
+	 * @param Updated $payload
+	 */
+	public function updated(Request $request, Updated $payload) {
+		$serializer = Action::getSerializer();
+		$relationship = $serializer->apis($payload->getModel());
+
+		return new JsonResponse($relationship->toArray());
+	}
+
+	/**
+	 */
+	protected function getPayloadMethods() {
+		return [
+			'keeko\framework\domain\payload\NotFound' => 'notFound',
+			'keeko\framework\domain\payload\NotValid' => 'notValid',
+			'keeko\framework\domain\payload\Updated' => 'updated',
+			'keeko\framework\domain\payload\NotUpdated' => 'notUpdated'
+		];
+	}
+}
